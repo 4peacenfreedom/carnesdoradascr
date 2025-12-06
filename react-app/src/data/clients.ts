6c19e783e55500ae4238a0f6859b9c0ececa5ccd
David Romero <4peacenfreedom@gmail.com>
2025-12-05 23:58:02 -0600

    updates en las imagenes de los logos de las empresas

--- react-app/src/data/clients.ts
+++ react-app/src/data/clients.ts
@@ -4,26 +4,31 @@ export const clients: Client[] = [
   {
     id: '1',
     name: 'Cliente 1',
-    logo: '/assets/images/resources/client-1-1.png',
+    logo: '/assets/images/resources/alof.webp',
   },
   {
     id: '2',
     name: 'Cliente 2',
-    logo: '/assets/images/resources/client-1-2.png',
+    logo: '/assets/images/resources/barcelo_oscuro.webp',
   },
   {
     id: '3',
     name: 'Cliente 3',
-    logo: '/assets/images/resources/client-1-3.png',
+    logo: '/assets/images/resources/crowne_oscuro.webp',
   },
   {
     id: '4',
     name: 'Cliente 4',
-    logo: '/assets/images/resources/client-1-4.png',
+    logo: '/assets/images/resources/radisson.webp',
   },
   {
     id: '5',
     name: 'Cliente 5',
-    logo: '/assets/images/resources/client-1-5.png',
+    logo: '/assets/images/resources/wyndham.webp',
+  },
+  {
+    id: '5',
+    name: 'Cliente 6',
+    logo: '/assets/images/resources/rodeo.webp',
   },
 ]
